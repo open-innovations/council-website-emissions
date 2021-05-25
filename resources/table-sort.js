050a8853fe16ed2d5a04eb684058256d01c23a4b
slowe <gravatar@strudel.org.uk>
2021-05-25 15:07:11 +0100

    Sort by text not HTML

--- resources/table-sort.js
+++ resources/table-sort.js
@@ -2,8 +2,8 @@
 table-sort-js
 Author: Lee Wannacott
 Licence: MIT License Copyright (c) 2021 Lee Wannacott 
-Modified by Stuart Lowe 28th January 2021
-    
+Modified by Stuart Lowe 25th May 2021 to sort by textContent rather than innerHTML
+	
 GitHub Repository: https://github.com/LeeWannacott/table-sort-js
 npm package: https://www.npmjs.com/package/table-sort-js
 Demo: https://leewannacott.github.io/Portfolio/#/GitHub
@@ -20,39 +20,39 @@ Instructions:
 */
 
 function tableSortJs() {
-    const columnData = [];
-    const dictOfColumnIndexAndTableRow = {};
-    for (let sortableTable of document.getElementsByTagName("table")) {
-        if (sortableTable.classList.contains("table-sort")) {
-            if (sortableTable.getElementsByTagName("thead").length === 0) {
-                const the = document.createElement("thead");
-                the.appendChild(sortableTable.rows[0]);
-                sortableTable.insertBefore(the, sortableTable.firstChild);
-            }
+	const columnData = [];
+	const dictOfColumnIndexAndTableRow = {};
+	for (let sortableTable of document.getElementsByTagName("table")) {
+		if (sortableTable.classList.contains("table-sort")) {
+			if (sortableTable.getElementsByTagName("thead").length === 0) {
+				const the = document.createElement("thead");
+				the.appendChild(sortableTable.rows[0]);
+				sortableTable.insertBefore(the, sortableTable.firstChild);
+			}
 
-            const tableHead = sortableTable.querySelector("thead");
-            const tableBody = sortableTable.querySelector("tbody");
-            const tableHeadHeaders = tableHead.querySelectorAll("th");
+			const tableHead = sortableTable.querySelector("thead");
+			const tableBody = sortableTable.querySelector("tbody");
+			const tableHeadHeaders = tableHead.querySelectorAll("th");
 
 
-            // Display a mouse pointer on hover over table headers.
-            tableHead.addEventListener("mouseover", function (event) {
-                setCursor(tableHead, "pointer");
-            });
-            function setCursor(tag, cursorStyle) {
-                var elem;
-                if (sortableTable.getElementsByTagName && (elem = tag)) {
-                    if (elem.style) {
-                        elem.style.cursor = cursorStyle;
-                    }
-                }
-            }
+			// Display a mouse pointer on hover over table headers.
+			tableHead.addEventListener("mouseover", function (event) {
+				setCursor(tableHead, "pointer");
+			});
+			function setCursor(tag, cursorStyle) {
+				var elem;
+				if (sortableTable.getElementsByTagName && (elem = tag)) {
+					if (elem.style) {
+						elem.style.cursor = cursorStyle;
+					}
+				}
+			}
 
-            for (let [columnIndex, th] of tableHeadHeaders.entries("table")) {
-                let timesClickedColumn = 0;
+			for (let [columnIndex, th] of tableHeadHeaders.entries("table")) {
+				let timesClickedColumn = 0;
 
-                th.addEventListener("click", function (e) {
-                    timesClickedColumn += 1;
+				th.addEventListener("click", function (e) {
+					timesClickedColumn += 1;
 
 					s = tableHead.querySelector('.sorted');
 					if(s){
@@ -62,92 +62,94 @@ function tableSortJs() {
 					}
 					e.target.classList.add(timesClickedColumn%2==0 ? 'sorted-up' : 'sorted-dn');
 					e.target.classList.add('sorted');
+					
+					function getText(tr,i){
+						el = tr.querySelectorAll("td").item(i);
+						return el.innerText || el.textContent
+					}
 
-                    function getTableDataOnClick() {
-                        const tableRows = tableBody.querySelectorAll("tr");
-                        for (let [i, tr] of tableRows.entries()) {
-                            if (
-                                tr.querySelectorAll("td").item(columnIndex)
-                                    .innerHTML !== ""
-                            ) {
-                                columnData.push(
-                                    tr.querySelectorAll("td").item(columnIndex)
-                                        .innerHTML +
-                                        "#" +
-                                        i
-                                );
-                                dictOfColumnIndexAndTableRow[
-                                    tr.querySelectorAll("td").item(columnIndex)
-                                        .innerHTML +
-                                        "#" +
-                                        i
-                                ] = tr.innerHTML;
-                            } else {
-                                // Fill in blank table cells with a value(0)
-                                columnData.push("0#" + i);
-                                dictOfColumnIndexAndTableRow["0#" + i] =
-                                    tr.innerHTML;
-                            }
-                        }
-                        function naturalSortAescending(a, b) {
-                            return a.localeCompare(
-                                b,
-                                navigator.languages[0] || navigator.language,
-                                { numeric: true, ignorePunctuation: true }
-                            );
-                        }
-                        function naturalSortDescending(a, b) {
-                            return naturalSortAescending(b, a);
-                        }
+					function getTableDataOnClick() {
+						const tableRows = tableBody.querySelectorAll("tr");
+						for(let [i, tr] of tableRows.entries()) {
+							if(
+								getText(tr,columnIndex) !== ""
+							){
+								columnData.push(
+									getText(tr,columnIndex) +
+										"#" +
+										i
+								);
+								dictOfColumnIndexAndTableRow[
+									getText(tr,columnIndex) +
+										"#" +
+										i
+								] = tr.innerHTML;
+							} else {
+								// Fill in blank table cells with a value(0)
+								columnData.push("0#" + i);
+								dictOfColumnIndexAndTableRow["0#" + i] =
+									tr.innerHTML;
+							}
+						}
+						function naturalSortAescending(a, b) {
+							return a.localeCompare(
+								b,
+								navigator.languages[0] || navigator.language,
+								{ numeric: true, ignorePunctuation: true }
+							);
+						}
+						function naturalSortDescending(a, b) {
+							return naturalSortAescending(b, a);
+						}
 
-                        // Sort naturally; default aescending unless th contains 'order-by-desc' as className.
-                        if (typeof columnData[0] !== "undefined") {
-                            if (
-                                th.classList.contains("order-by-desc")  &&
-                                timesClickedColumn === 1
-                            ) {
-                                columnData.sort(naturalSortDescending, {
-                                    numeric: true,
-                                    ignorePunctuation: true,
-                                });
-                            } else if (
-                                th.classList.contains("order-by-desc")  &&
-                                timesClickedColumn === 2
-                            ) {
-                                columnData.sort(naturalSortAescending, {
-                                    numeric: true,
-                                    ignorePunctuation: true,
-                                });
-                                timesClickedColumn = 0;
-                            } else if (timesClickedColumn === 1) {
-                                columnData.sort(naturalSortAescending);
-                            } else if (timesClickedColumn === 2) {
-                                columnData.sort(naturalSortDescending);
-                                timesClickedColumn = 0;
-                            }
-                        }
-                    }
-                    getTableDataOnClick();
-                    function returnSortedTable() {
-                        const tableRows = tableBody.querySelectorAll("tr");
-                        for (let [i, tr] of tableRows.entries()) {
-                            tr.innerHTML =
-                                dictOfColumnIndexAndTableRow[columnData[i]];
-                        }
-                        columnData.length = 0;
-                    }
-                    returnSortedTable();
-                });
-            }
-        }
-    }
+						// Sort naturally; default aescending unless th contains 'order-by-desc' as className.
+						if (typeof columnData[0] !== "undefined") {
+							if (
+								th.classList.contains("order-by-desc")  &&
+								timesClickedColumn === 1
+							) {
+								columnData.sort(naturalSortDescending, {
+									numeric: true,
+									ignorePunctuation: true,
+								});
+							} else if (
+								th.classList.contains("order-by-desc")  &&
+								timesClickedColumn === 2
+							) {
+								columnData.sort(naturalSortAescending, {
+									numeric: true,
+									ignorePunctuation: true,
+								});
+								timesClickedColumn = 0;
+							} else if (timesClickedColumn === 1) {
+								columnData.sort(naturalSortAescending);
+							} else if (timesClickedColumn === 2) {
+								columnData.sort(naturalSortDescending);
+								timesClickedColumn = 0;
+							}
+						}
+					}
+					getTableDataOnClick();
+					function returnSortedTable() {
+						const tableRows = tableBody.querySelectorAll("tr");
+						for (let [i, tr] of tableRows.entries()) {
+							tr.innerHTML =
+								dictOfColumnIndexAndTableRow[columnData[i]];
+						}
+						columnData.length = 0;
+					}
+					returnSortedTable();
+				});
+			}
+		}
+	}
 }
 
 if (
-    document.readyState === "complete" ||
-    document.readyState === "interactive"
+	document.readyState === "complete" ||
+	document.readyState === "interactive"
 ) {
-    tableSortJs();
+	tableSortJs();
 } else if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", tableSortJs, false);
+	document.addEventListener("DOMContentLoaded", tableSortJs, false);
 }
