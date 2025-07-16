2e5109c3624968be01656619c66a88a0f7d93f5e
slowe <299787+slowe@users.noreply.github.com>
2025-07-16 21:47:00 +0100

    Update for July 2025

--- resources/table-sort.js
+++ resources/table-sort.js
@@ -1,163 +1,245 @@
-/* 
-table-sort-js
-Author: Lee Wannacott
-Licence: MIT License Copyright (c) 2021 Lee Wannacott 
-Modified by Stuart Lowe:
-  * 2025-01-07: keep row classes
-  * 2021-05-25: sort by textContent rather than innerHTML
-	
-GitHub Repository: https://github.com/LeeWannacott/table-sort-js
-npm package: https://www.npmjs.com/package/table-sort-js
-Demo: https://leewannacott.github.io/Portfolio/#/GitHub
-
-Install:
-Frontend: <script src="https://leewannacott.github.io/table-sort-js/table-sort.js"></script> or
-Download this file and add <script src="table-sort.js"></script> to your HTML 
-
-Backend: npm install table-sort-js and use require("../node_modules/table-sort-js/table-sort.js") 
-
-Instructions:
-  Add class="table-sort" to tables you'd like to make sortable
-  Click on the table headers to sort them.
+/*
+	Open Innovations Sortable Tables v0.3.3
+	Helper function to make any table with class="table-sort" sortable.
+	We would have used https://github.com/leewannacott/table-sort-js/ but it couldn't deal with merged rows
 */
+(function(root){
 
-function tableSortJs() {
-	const columnData = [];
-	const dictOfColumnIndexAndTableRow = {};
-	const rowClasses = {};
-	for (let sortableTable of document.getElementsByTagName("table")) {
-		if (sortableTable.classList.contains("table-sort")) {
-			if (sortableTable.getElementsByTagName("thead").length === 0) {
-				const the = document.createElement("thead");
-				the.appendChild(sortableTable.rows[0]);
-				sortableTable.insertBefore(the, sortableTable.firstChild);
+	var styles = document.createElement('style');
+	styles.innerHTML = '';
+	document.head.prepend(styles);
+
+	if(!root.OI) root.OI = {};
+	if(!root.OI.ready){
+		root.OI.ready = function(fn){
+			// Version 1.1
+			if(document.readyState != 'loading') fn();
+			else document.addEventListener('DOMContentLoaded', fn);
+		};
+	}
+	function findMatch(arr,el){
+		for(var i = 0; i < arr.length; i++){
+			if(arr[i]==el) return i;
+		}
+		return -1;
+	}
+	function clone(obj){
+		return JSON.parse(JSON.stringify(obj));
+	}
+	function normaliseTable(table){
+
+		var r,c,i,ncol,thead,tbody,tr,rows,cols,o,span,cells,hrows,row,id,coltype,v,best;
+
+		o = {};
+
+		tbody = table.querySelector('tbody');
+		thead = table.querySelector('thead');
+		rows = [];
+		hrows = [];
+		cells = [];
+
+		if(thead){
+			tr = thead.querySelectorAll('tr');
+			for(r = 0; r < tr.length; r++) hrows.push(tr[r]);
+			tr = tbody.querySelectorAll('tr');
+			for(r = 0; r < tr.length; r++) rows.push(tr[r]);
+		}else{
+			thead = document.createElement('thead');
+			table.prepend(thead);
+
+			if(!tbody){
+				tbody = document.createElement('tbody');
+				table.appendChild(tbody);
 			}
 
-			const tableHead = sortableTable.querySelector("thead");
-			const tableBody = sortableTable.querySelector("tbody");
-			const tableHeadHeaders = tableHead.querySelectorAll("th");
+			tr = (tbody ? tbody : table).querySelectorAll('tr');
+			for(r = 0; r < tr.length; r++){
+				if(tr[r].querySelectorAll('th').length > 0){
+					hrows.push(tr[r]);
+					thead.appendChild(tr[r]);
+				}else{
+					rows.push(tr[r]);
+					tbody.appendChild(tr[r]);
+				}
+			}
+		}
 
+		ncol = hrows[0].querySelectorAll('td,th').length;
+		var rowspan = new Array(ncol);
+		for(c = 0; c < ncol; c++) rowspan[c] = 0;
+		var mtable = new Array(rows.length);
+		var coltype = new Array(ncol);
+		var cls = "";
 
-			// Display a mouse pointer on hover over table headers.
-			tableHead.addEventListener("mouseover", function (event) {
-				setCursor(tableHead, "pointer");
-			});
-			function setCursor(tag, cursorStyle) {
-				var elem;
-				if (sortableTable.getElementsByTagName && (elem = tag)) {
-					if (elem.style) {
-						elem.style.cursor = cursorStyle;
+		// Convert into an array
+		for(r = 0; r < rows.length; r++){
+			cols = rows[r].querySelectorAll('td,th');
+			rows[r] = [];
+			mtable[r] = [];
+			for(c = 0,i = 0; c < cols.length; c++){
+				rows[r].push(cols[c]);
+				cells.push(cols[c]);
+			}
+			for(c = 0; c < ncol; c++) mtable[r].push({});
+		}
+
+		for(r = 0; r < rows.length; r++){
+			row = [];
+			cls = rows[r][0].closest('tr').getAttribute('class');
+			for(c = 0,i = 0; c < ncol; c++){
+				if(rowspan[c] > 0){
+					// Keep a reference to the previous row/col
+					mtable[r][c] = clone(mtable[r-1][c]);
+					rowspan[c]--;
+				}else{
+					// Check the col
+					span = rows[r][i].getAttribute('rowspan');
+					if(typeof span==="string"){
+						span = parseInt(span);
+						rowspan[c] = span-1;
+					}
+					id = findMatch(cells,rows[r][i])
+					mtable[r][c] = {'id':id,'v':cells[id].getAttribute('data-sort')||cells[id].innerText};
+					if(cls) mtable[r][c].class = cls;
+					if(!coltype[c]) coltype[c] = {};
+					i++;
+				}
+				v = mtable[r][c].v;
+				typ = "string";
+				// Remove any quotes around the column value
+				v = v.replace(/^\"|\"$/g,"");
+				if(v.match(/^[\-\+0-9\,]+\.[0-9]+$/)) typ = "float";
+				else if(v.match(/^([0-9]+|[0-9\,]+\,[0-9]{3})$/)) typ = "integer";
+				else if(!isNaN(Date.parse(v))) typ = "datetime";
+				else if(v.match(/^[A-Z][\+\*]?$/)) typ = "grade";
+				else if(v=="") typ = "empty";
+				if(!coltype[c][typ]) coltype[c][typ] = 0;
+				coltype[c][typ]++;
+
+			}
+		}
+		for(c = 0; c < ncol; c++){
+			for(typ in coltype[c]){
+				// If the type + empty values is more than 80% of the column we use this type
+				if(coltype[c][typ] + (coltype[c].empty||0) >= 0.8*rows.length){
+					for(r = 0; r < rows.length; r++){
+						v = mtable[r][c].v;
+						if(typ=="float") v = parseFloat(v.replace(/,/g,'')||'0');
+						if(typ=="integer") v = parseInt(v.replace(/,/g,'')||'0');
+						if(typ=="datetime") v = (new Date(v)).getTime();
+						mtable[r][c].v = v;
+						mtable[r][c].type = typ;
 					}
 				}
 			}
+		}
 
-			for (let [columnIndex, th] of tableHeadHeaders.entries("table")) {
-				let timesClickedColumn = 0;
+		o.cols = ncol;
+		o.rows = rows.length;
+		o.cells = cells;
+		o.table = mtable;
+		o.body = tbody;
+		o.head = thead;
+		o.header = new Array(ncol);
+		for(c = 0; c < ncol; c++) o.header[c] = hrows[0].querySelectorAll('td,th')[c];
+		return o;
+	}
 
-				th.addEventListener("click", function (e) {
-					timesClickedColumn += 1;
+	function buildTable(el,table){
 
-					s = tableHead.querySelector('.sorted');
-					if(s){
-						s.classList.remove('sorted');
-						s.classList.remove('sorted-dn');
-						s.classList.remove('sorted-up');
-					}
-					e.target.classList.add(timesClickedColumn%2==0 ? 'sorted-up' : 'sorted-dn');
-					e.target.classList.add('sorted');
-					
-					function getText(tr,i){
-						el = tr.querySelectorAll("td").item(i);
-						return el.innerText || el.textContent
-					}
+		var r,tr,id,cls;
+		var cells = table.cells;
+		var newbody = document.createElement('tbody');
 
-					function getTableDataOnClick() {
-						const tableRows = tableBody.querySelectorAll("tr");
-						for(let [i, tr] of tableRows.entries()) {
-							rowClasses[i] = tr.getAttribute('class');
-							if(
-								getText(tr,columnIndex) !== ""
-							){
-								columnData.push(
-									getText(tr,columnIndex) +
-										"#" +
-										i
-								);
-								dictOfColumnIndexAndTableRow[
-									getText(tr,columnIndex) +
-										"#" +
-										i
-								] = tr.innerHTML;
-							} else {
-								// Fill in blank table cells with a value(0)
-								columnData.push("0#" + i);
-								dictOfColumnIndexAndTableRow["0#" + i] =
-									tr.innerHTML;
-							}
-						}
-						function naturalSortAescending(a, b) {
-							return a.localeCompare(
-								b,
-								navigator.languages[0] || navigator.language,
-								{ numeric: true, ignorePunctuation: true }
-							);
-						}
-						function naturalSortDescending(a, b) {
-							return naturalSortAescending(b, a);
-						}
+		// Build the sorted table
+		for(var r = 0; r < table.rows; r++){
+			tr = document.createElement('tr');
+			if(table.table[r][0].class) tr.setAttribute('class',table.table[r][0].class);
+			for(var c = 0; c < table.cols; c++){
+				id = table.table[r][c].id;
+				td = cells[id].cloneNode(true);
+				td.removeAttribute('rowspan');
+				tr.appendChild(td);
+			}
+			newbody.appendChild(tr);
+		}
+		table.body.parentNode.replaceChild(newbody,table.body);
+		table.body = newbody;
+	}
 
-						// Sort naturally; default aescending unless th contains 'order-by-desc' as className.
-						if (typeof columnData[0] !== "undefined") {
-							if (
-								th.classList.contains("order-by-desc")  &&
-								timesClickedColumn === 1
-							) {
-								columnData.sort(naturalSortDescending, {
-									numeric: true,
-									ignorePunctuation: true,
-								});
-							} else if (
-								th.classList.contains("order-by-desc")  &&
-								timesClickedColumn === 2
-							) {
-								columnData.sort(naturalSortAescending, {
-									numeric: true,
-									ignorePunctuation: true,
-								});
-								timesClickedColumn = 0;
-							} else if (timesClickedColumn === 1) {
-								columnData.sort(naturalSortAescending);
-							} else if (timesClickedColumn === 2) {
-								columnData.sort(naturalSortDescending);
-								timesClickedColumn = 0;
-							}
-						}
-					}
-					getTableDataOnClick();
-					function returnSortedTable() {
-						const tableRows = tableBody.querySelectorAll("tr");
-						for (let [i, tr] of tableRows.entries()) {
-							let m = columnData[i].match(/(.*)\#(.*)/);
-							let j = parseInt(m[2]);
-							tr.innerHTML =
-								dictOfColumnIndexAndTableRow[columnData[i]];
-							// Update row class with old class
-							tr.setAttribute('class',rowClasses[j]||"");
-						}
-						columnData.length = 0;
-					}
-					returnSortedTable();
-				});
+	function SortableColumn(el,table,c,attr){
+		if(!attr) attr = {};
+		var dir = "";
+		var indicator = table.header[c].querySelector('.oi-table-dir');
+		if(!indicator){
+			indicator = document.createElement('span');
+			indicator.classList.add('oi-table-dir');
+			table.header[c].appendChild(indicator);
+			table.header[c].style.cursor = 'pointer';
+		}
+		this.getDirection = function(){
+			return dir;
+		};
+		this.setDirection = function(d){
+			dir = d;
+			var v = "";//•";
+			if(dir=="up") v = "▲";
+			if(dir=="down") v = "▼";
+			indicator.innerHTML = v;
+			return this;
+		};
+		this.toggleDirection = function(){
+			this.setDirection(!dir || dir=="up" ? "down" : "up");
+			return dir;
+		};
+		
+		table.header[c].addEventListener('click',function(e){
+			if(attr._parent) attr._parent.sortColumn(c);
+		});
+
+		this.setDirection(dir);
+		return this;
+	}
+
+	function SortableTable(el,attr){
+		if(!attr) attr = {};
+		var table = normaliseTable(el);
+		this.columns = new Array(table.cols);
+		this.sortColumn = function(col){
+			dir = this.columns[col].toggleDirection();
+
+			table.table = table.table.sort(function(a,b){
+				if(a[col].v == b[col].v) return false;
+				var r = (a[col].v > b[col].v ? 1 : -1)
+				if(a[col].type=="grade" &&  b[col].type=="grade"){
+					if(a[col].v==b[col].v+"+") r = -1;
+					if(b[col].v==a[col].v+"+") r = 1;
+				}
+				return (dir == "up" ? -1: 1)*r;
+			});
+			// Loop over columns changing indicators
+			for(var c = 0; c < this.columns.length; c++){
+				if(c!=col) this.columns[c].setDirection("");
+			}
+			buildTable(el,table);
+			if(typeof attr.onsort==="function") attr.onsort.call(this);
+		}
+
+		if(table.rows > 0){
+			// Add functions to columns
+			for(var c = 0; c < table.cols; c++){
+				if(!table.header[c].getAttribute('disable-sort')) this.columns[c] = new SortableColumn(el,table,c,{'_parent':this});
 			}
 		}
-	}
-}
 
-if (
-	document.readyState === "complete" ||
-	document.readyState === "interactive"
-) {
-	tableSortJs();
-} else if (document.readyState === "loading") {
-	document.addEventListener("DOMContentLoaded", tableSortJs, false);
-}
+		return this;
+	}
+	
+	OI.TableSort = function(e,attr){ return new SortableTable(e,attr); }
+
+	OI.ready(function(){
+		var tables = document.querySelectorAll('.table-sort');
+		for(var t = 0; t < tables.length; t++) new SortableTable(tables[t]);
+	});
+
+})(window || this);
